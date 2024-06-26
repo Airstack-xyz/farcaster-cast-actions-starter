3b056557f566526eeb72daca91236adfb3e05176
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2024-06-26 14:31:03 +0400

    update code to be able to run locally

--- api/index.tsx
+++ api/index.tsx
@@ -6,7 +6,6 @@ import {
 import { devtools } from "@airstack/frog/dev";
 import { serveStatic } from "@airstack/frog/serve-static";
 import { handle } from "@airstack/frog/vercel";
-import { gm } from "../lib/gm.js";
 import { config } from "dotenv";
 
 config();
@@ -22,24 +21,23 @@ export const app = new Frog({
 
 // Cast action handler
 app.hono.post("/gm", async (c) => {
-  console.log(c);
-  const body = await c.req.json();
-
-  const { isValid, message } = await validateFramesMessage(body);
-  const interactorFid = message?.data?.fid;
-  const castFid = message?.data.frameActionBody.castId?.fid as number;
+  const { isValid, message } =
+    process.env.NODE_ENV === "development"
+      ? { isValid: true, message: {} }
+      : await validateFramesMessage(await c.req.json?.());
+  const interactorFid = message?.data?.fid ?? 0;
+  const castFid = message?.data?.frameActionBody?.castId?.fid ?? 1;
   if (isValid) {
     if (interactorFid === castFid) {
       return c.json({ message: "Nice try." }, 400);
     }
-
-    await gm(castFid);
 
     const { data, error } = await getFarcasterUserDetails({
       fid: castFid,
     });
 
     if (error) {
+      console.error(error);
       return c.json({ message: "Error. Try Again." }, 500);
     }
 
